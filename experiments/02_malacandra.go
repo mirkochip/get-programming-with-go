24549b11a1a0f17c88ac700261c2cd780504f92e
mirkochip <mirkochip@gmail.com>
2023-02-21 17:57:20 +0100

    [enhancement] Externalized speedCalculator() function (testing purpose)

--- experiments/02_malacandra.go
+++ experiments/02_malacandra.go
@@ -2,11 +2,15 @@ package experiments
 
 import "fmt"
 
+// speedCalculator calculates the speed (Km/h) given a distance (Km) and a duration (hh)
+func speedCalculator(distance float64, duration float64) float64 {
+	return distance / duration
+}
+
 // Malacandra finds the travel speed to reach Mars in 28 days,
 // provided that it's 56.000.000 Km far away from the Earth
 func Malacandra() {
 	d := 56 * 1e6
-	t := 28
-	v := d / (float64)(t*24)
-	fmt.Printf("Reaching Malacandra in 28 days travelling at just %f Km/h.\n", v)
+	t := 28.0
+	fmt.Printf("Malacandra is reacheable in 28 days travelling at %f Km/h.\n", speedCalculator(d, t*24))
 }
