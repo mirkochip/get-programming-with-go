1e2c9bcfd7b274ac3d0fbb21cf1569c47a024ecc
mirkochip <mirkochip@gmail.com>
2023-03-23 10:27:08 +0100

    [feature] Piggy Int experiment completed

--- experiments/07_piggy_int.go
+++ experiments/07_piggy_int.go
@@ -1,5 +1,38 @@
 package experiments
 
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func getRandomIntCoin() int {
+	randomValue := rand.Intn(3)
+	randomCoin := 0
+	switch randomValue {
+	case 0:
+		randomCoin = 5
+	case 1:
+		randomCoin = 10
+	case 2:
+		randomCoin = 20
+	}
+	return randomCoin
+}
+
 func PiggyInt() {
+	var savingsTarget = 20 * 100
+	var currentSavings = 0
+	for {
+		coin := getRandomIntCoin()
+		fmt.Printf("Introducing %v USD cents.\n", coin)
+		currentSavings += coin
+		fmt.Printf("Current balance: %.2f USD.\n", float64(currentSavings)/100)
+		time.Sleep(time.Second / 2)
+		if currentSavings >= savingsTarget {
+			fmt.Printf("Savings target of %v USD just reached.\n", savingsTarget/100)
+			break
+		}
+	}
 
 }
